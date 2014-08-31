2d044fafe12ed109099bd48c3174f379e7bfcdef
Rajiv Bose <rbose85@users.noreply.github.com>
2014-08-31 20:58:00 +0100

    add validation logic for contact form

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -3,20 +3,42 @@
 
     function ContactUs($scope) {
         $scope.name = 'Potato';
-        $scope.error = {
-            name: undefined,
-            email: undefined,
-            company: undefined,
-            message: undefined
+    }
+
+    function ContactForm($scope) {
+        $scope.save = function () {
+            $scope.$broadcast('show-errors-check-validity');
+
+            if ($scope.contactForm.$valid) {
+                // todo: post to db
+                console.log('Validation passed -- form: ', $scope.contactForm);
+                $scope.reset();
+            }
+        };
+
+        $scope.reset = function () {
+            $scope.$broadcast('show-errors-reset');
+            $scope.model = {
+                name: '',
+                email: '',
+                company: '',
+                message: '',
+                subscribe: ''
+            };
         };
     }
 
     angular
         .module('hutchhouseApp.controllers', [
-            'ui.bootstrap'
+            'ui.bootstrap',
+            'ui.bootstrap.showErrors'
         ])
         .controller('ContactController', [
             '$scope',
             ContactUs
+        ])
+        .controller('CorrespondenceController', [
+            '$scope',
+            ContactForm
         ]);
 })();
